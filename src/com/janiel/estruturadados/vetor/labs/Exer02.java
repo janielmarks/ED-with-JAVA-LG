aedbe03ffff8f3dc0c0f7f1f88e5e0e2dfd46f79
Janiel Marques <janielmarques@icloud.com>
2024-12-27 22:33:06 -0300

    Correção do Exer02

---
+++ src/com/janiel/estruturadados/vetor/labs/Exer02.java
@@ -0,0 +1,25 @@
+package com.janiel.estruturadados.vetor.labs;
+
+import com.janiel.estruturadados.vetor.Lista;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exer02 {
+    public static void main(String[] args) {
+
+        ArrayList<String> arrayList = new ArrayList<String>(5);
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+
+        System.out.println(arrayList.lastIndexOf("A"));
+
+        Lista<String> lista = new Lista<String>(5);
+        lista.adiciona("A");
+        lista.adiciona("B");
+        lista.adiciona("A");
+
+        System.out.println(lista.ultimoIndice("A"));
+    }
+}
