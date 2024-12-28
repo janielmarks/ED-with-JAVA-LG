43b4acbe98a2476e381977fbc46ac9ebeebea6ac
Janiel Marques <janielmarques@icloud.com>
2024-12-27 23:42:55 -0300

    Resolução do Exer05

--- src/com/janiel/estruturadados/vetor/Lista.java
+++ src/com/janiel/estruturadados/vetor/Lista.java
@@ -118,6 +118,22 @@ public class Lista<T> {
             this.remove(pos);
         }
     }
+    public void limpar(){
+
+
+        //opcao 1
+        this.elementos = (T[]) new Object[this.elementos.length];
+
+        // opcao 2
+        this.tamanho = 0;
+
+        //opcao 3
+        for(int i=0; i<this.tamanho; i++){
+            this.elementos[i] = null;
+        }
+        this.tamanho = 0;
+    }
+
 
     public int tamanho(){
         return this.tamanho;
