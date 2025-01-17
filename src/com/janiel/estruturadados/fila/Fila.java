357e5ef4316916da02b12a6b736ceb01147fafba
Janiel Marques <janielmarques@icloud.com>
2025-01-17 09:18:16 -0300

    Aula19 - Aula introdutorio sobre filas

---
+++ src/com/janiel/estruturadados/fila/Fila.java
@@ -0,0 +1,15 @@
+package com.janiel.estruturadados.fila;
+
+import base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+
+    public Fila(){
+
+    }
+    public Fila(int capacidade){
+        super (capacidade);
+    }
+
+
+}
