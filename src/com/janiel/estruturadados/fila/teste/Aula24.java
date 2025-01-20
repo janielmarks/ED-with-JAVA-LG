49433791735299cc154258a61dd75f561b6b8826
Janiel Marques <janielmarques@icloud.com>
2025-01-20 09:15:53 -0300

    Exer24 -implementação do  Metodo filas com prioridades

---
+++ src/com/janiel/estruturadados/fila/teste/Aula24.java
@@ -0,0 +1,19 @@
+package com.janiel.estruturadados.fila.teste;
+
+import com.janiel.estruturadados.fila.FilaComPrioridade;
+
+public class Aula24 {
+    public static void main(String[] args) {
+
+        FilaComPrioridade<Paciente> fila = new FilaComPrioridade<>();
+
+        fila.enfileira(new Paciente("A", 2));
+        fila.enfileira(new Paciente("C", 1));
+        fila.enfileira(new Paciente("B", 3));
+
+        System.out.println(fila); //1 2 3 e não 1 3 2
+        System.out.println(fila.desenfileira());
+    }
+
+
+}
