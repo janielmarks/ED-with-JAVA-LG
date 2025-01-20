94ce14d49c73c5bca90a0404c09b37effaab67fe
Janiel Marques <janielmarques@icloud.com>
2025-01-20 14:44:27 -0300

    Introdução a ListasEncadeadas

---
+++ src/com/janiel/estruturadados/lista/teste/ListaEncadeadaTeste.java
@@ -0,0 +1,15 @@
+package com.janiel.estruturadados.lista.teste;
+
+import com.janiel.estruturadados.lista.ListaEncadeada;
+
+public class ListaEncadeadaTeste {
+    public static void main(String[] args) {
+         ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.adiciona(1);
+
+        System.out.println(lista);
+
+        
+    }
+    
+}
