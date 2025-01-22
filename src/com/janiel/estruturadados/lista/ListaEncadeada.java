b62b684e735b5e67554bac895c3cfcc802d0a095
Janiel Marques <janielmarques@icloud.com>
2025-01-22 09:55:48 -0300

    Adicionado o método percorrer a lista

--- src/com/janiel/estruturadados/lista/ListaEncadeada.java
+++ src/com/janiel/estruturadados/lista/ListaEncadeada.java
@@ -26,9 +26,31 @@ public class ListaEncadeada<T> {
     }
     @Override
     public String toString() {
-        return "ListaEncadeada [inicio=" + inicio + "]";
-    }
-    
 
-    
+        // [1, 2, 3, 4]
+
+        if (this.tamanho == 0) {
+            return "[]";
+        }
+
+        StringBuilder builder = new StringBuilder("[");
+        // builder.append("ListaEncadeada [inicio=").append(inicio).append("]");
+
+        No<T> atual = this.inicio;
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            builder.append(atual.getElemento()).append(",");
+            atual = atual.getProximo();
+        }
+        builder.append(atual.getElemento()).append("]");
+        /*
+         * builder.append(atual.getElemento()).append(",");
+         * while (atual.getProximo() != null) {
+         * atual = atual.getProximo();
+         * builder.append(atual.getElemento()).append(",");
+         * }
+         */
+
+        return builder.toString();
+    }
+
 }
